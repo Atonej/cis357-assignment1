0f69b7e96ebdfd0ccc4529fffa7b5ba15cabefa2
Workbook-05 <workbook05@workbook-05s-macbook-pro.local>
2018-09-17 13:17:14 -0400

    Initial Commit

---
+++ conversionCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  conversionCalc
+//
+//  Created by Workbook-05 on 9/17/18.
+//  Copyright © 2018 Atone-Anthony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
